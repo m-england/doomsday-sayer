cb6090c3fc47d410ca4ab95a2bb66d6b5a6369e4
Mark England <me@markeng.land>
2025-01-27 21:10:54 -0600

    Add files via upload

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://m-england.github.io/doomsday-sayer/sw.js', { scope: 'https://m-england.github.io/doomsday-sayer/' })})}
